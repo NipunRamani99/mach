50713bf1de0d9ead7f37c29a1a5379aeb6b4c586
Nipun Ramani <2822656r@student.gla.ac.uk>
2023-08-15 21:28:30 +0100

    Added Narrowphase test

--- engine/test/unit_test/MachTest.cpp
+++ engine/test/unit_test/MachTest.cpp
@@ -26,3 +26,28 @@ TEST(MachTest, BroadphaseTest) {
 	ASSERT_EQ(collisionPairs.size(), 1);
 }
 
+//Test Narrowphase
+TEST(MachTest, NarrowPhase) {
+	Mach mach;
+	BoxRigidBody boxA = BoxRigidBody();
+	boxA.size = glm::vec2(70.0f, 70.0f);
+	boxA.angle = 0.0f;
+	boxA.position = glm::vec2(100, 100);
+	boxA.calculateAABB();
+
+	BoxRigidBody boxB = BoxRigidBody();
+	boxB.size = { 80.0f, 80.0f };
+	boxB.angle = 0.0f;
+	boxB.position = { 110,110 };
+	boxB.calculateAABB();
+
+	mach.addDynamicObject(&boxA);
+	mach.addDynamicObject(&boxB);
+
+	mach.broadPhase();
+
+	mach.narrowPhase();
+	auto contactList = mach.getContactList();
+	ASSERT_EQ(contactList.size(), 1);
+}
+
