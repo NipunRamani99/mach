e9cc15de8e45b9b3316f9ec4c0378dfe35ee6721
Nipun Ramani <2822656r@student.gla.ac.uk>
2023-09-05 13:33:33 +0100

    Final Commit

--- sandbox/src/SceneManager.hpp
+++ sandbox/src/SceneManager.hpp
@@ -5,6 +5,7 @@
 #include "RevoluteJointScene.hpp"
 #include "PyramidScene.hpp"
 #include "CarScene.hpp"
+#include "TrainScene.hpp"
 #include "include/mach.hpp"
 #include <memory>
 #include <SFML/Graphics.hpp>
@@ -13,6 +14,7 @@ class SceneManager {
 private:
 	std::unique_ptr<Scene> currentScene;
 	Mach& mach;
+	size_t currentSceneNumber = 0;
 public:
 	SceneManager(Mach & mach) 
 		:
@@ -27,23 +29,37 @@ public:
 			if (currentScene) currentScene->teardown();
 			currentScene = std::make_unique<DefaultScene>(mach);
 			currentScene->initialize();
+			currentSceneNumber = 0;
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2)) {
 			if (currentScene) currentScene->teardown();
 			currentScene = std::make_unique<RevoluteJointScene>(mach);
 			currentScene->initialize();
+			currentSceneNumber = 1;
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3)) {
 			if (currentScene) currentScene->teardown();
 			currentScene = std::make_unique<PyramidScene>(mach);
 			currentScene->initialize();
+			currentSceneNumber = 2;
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4)) {
 			if (currentScene) currentScene->teardown();
 			currentScene = std::make_unique<CarScene>(mach);
 			currentScene->initialize();
+			currentSceneNumber = 3;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num5)) {
+			if (currentScene) currentScene->teardown();
+			currentScene = std::make_unique<TrainScene>(mach);
+			currentScene->initialize();
+			currentSceneNumber = 4;
 		}
 		currentScene->processInput(window);
+	}
+
+	size_t getCurrentSceneNumber() {
+		return currentSceneNumber;
 	}
 
 	~SceneManager() {
